9a4319e5782a1998cfa54ae51b014b289dec9816
Developer <dev@jump-jump.local>
2026-05-31 23:37:21 +0800

    feat: add GameState autoload with state machine and score

---
+++ scripts/game_state.gd
@@ -0,0 +1,22 @@
+extends Node
+
+enum State { MAIN_MENU, PLAYING, GAME_OVER }
+
+signal state_changed(new_state: State)
+signal score_changed(new_score: int)
+signal score_up(landing_pos: Vector3)
+
+var current_state: State = State.MAIN_MENU
+var score: int = 0
+
+func change_state(new_state: State) -> void:
+	current_state = new_state
+	state_changed.emit(new_state)
+
+func add_score() -> void:
+	score += 1
+	score_changed.emit(score)
+
+func reset_score() -> void:
+	score = 0
+	score_changed.emit(0)
